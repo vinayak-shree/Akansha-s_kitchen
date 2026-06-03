211918e38023301584027f48b91ae4a003897002
vinayak-shree <142142244+vinayak-shree@users.noreply.github.com>
2026-06-03 09:37:11 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Recepie(models.Model):
+    user = models.ForeignKey(User , on_delete=models.SET_NULL , null = True)
+    recepie_name = models.CharField(max_length=100)
+    recepie_description = models.TextField()
+    recepie_image = models.ImageField(upload_to = "recepie")
+    recepie_category = models.CharField(max_length = 100 , null = True , blank=True)
+    recepie_link = models.CharField(null = True , blank=True)
